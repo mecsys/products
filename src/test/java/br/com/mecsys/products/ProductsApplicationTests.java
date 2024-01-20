ed9fa1359d69833e370ded2d987476b6a5f2c6be
Isaac Mecchi <isaac.mecchi@sensedia.com>
2024-01-20 15:12:25 -0300

    Initial commit.

---
+++ src/test/java/br/com/mecsys/products/ProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mecsys.products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
